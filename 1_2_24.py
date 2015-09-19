2034bca55ee5182faa0e439ee7e7bd4a46b3b6ab
Anna Nuxoll <nuxoll.anna@gmail.com>
2015-09-19 12:02:48 -0700

    test that distribution is gaussian

--- 1_2_24.py
+++ 1_2_24.py
@@ -1,25 +1,23 @@
-#Gaussian random numbers 
+# Gaussian random numbers 
 import math
 import random
 import sys
 
 # def gaussian_random_num():
 # 	u = random.random()
-#     v = random.random()
-#     w1 = math.sin(2 * pi * v)
-#     w2 = math.sqrt(-2 * ln_u)
-#     w = w1 * w2
-#     print w
+#   v = random.random()
+#   w1 = math.sin(2 * pi * v)
+#   w2 = math.sqrt(-2 * ln_u)
+#   w = w1 * w2
 
 # Box-Muller formula: w = sin(2 pi v) (-2 ln u)^1/2
-#set random numbers u and v
+# set random numbers u and v
 # I need to redo this with list comprehensions, probably
 
 num = int(sys.argv[1])
 def gaussian_random_num_list(num):
     w_list = []
     pi = math.pi
-    e = math.e
     for i in range(num):
         u = random.random()
         v = random.random()
@@ -29,41 +27,34 @@ def gaussian_random_num_list(num):
         w2 = math.sqrt(-2 * ln_u)
         w = w1 * w2
         w_list.append(w)
-    total = 0
+    
+    total = 0.0
     item = 0
-    import pdb; pdb.set_trace()
     while item <= (num-1): 
 	    total += w_list[item]
 	    item += 1
 
-    print "When %d numbers are generated, the average is %d" % (num, total/num)
-    print "The highest is %d" % max(w_list)
-    print "The lowest is %d" % min(w_list)
-    # want to test that it actually generates gaussian random numbers 
-    new_list = [0*x for x in range(num)]
+    print "When %d numbers are generated, the average is %r" % (num, total/num)
+    print "The highest is %r" % max(w_list)
+    print "The lowest is %r" % min(w_list)
+
+    # test that it actually generates gaussian random numbers 
+    # find the difference of each number from the mean, square that value
+    new_list = []
     count = 0
     while count <= (num-1): 
-	    new_item = (total/num - w_list[count]) ** 2 
-	    new_list[count] = new_item
+	    new_list.append((total/num - w_list[count]) ** 2)
 	    count += 1
     
-    new_total = 0 
+    # find the average of the deviations squared (the variance)
+    new_total = 0.0
     item = 0
     while item <= (num-1): 
 	    new_total += new_list[item]
 	    item += 1
-
-    print "The standard deviation is %d" % math.sqrt(new_total)
-
-    # new_total = 0
-    # item2 = 0
-    # while item2 <= (num-1): 
-	   #  new_total += new_list[item2]
-	   #  item2 += 1
-
-
-
-
+    # the standard deviation is the square root of the variance
+    print "The expected std dev is 1"
+    print "The measured std dev is %r" % math.sqrt(new_total/num)
 
 
 gaussian_random_num_list(num)
